264a69e6df738e7923ada89e23eb62251a93edb4
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-06-27 11:13:11 +0300

    useref-play1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import logo from './logo.svg';
+// useState vs useRef
+// when a variable changed, useState triggeers the componnet and render process works again
+// but with useRef,  trigger of rendering can prevent.
+import { useRef, useState } from 'react';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
+  //const [counter, setCounter] = useState(0);
+
+  //const count = useRef(0);
+
+  // const onClick = () => {
+  //   setCounter(counter + 1);
+  // }
+
+  // const styles = {
+  //   main: {
+  //     color: "red"
+  //   }
+  // }
+
+  const videoRef = useRef(null);
+
+  const handlePlay = () => {
+    videoRef.current.play();
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+    {/*  {counter}r */}  
+    {/* useState we can show count:1 on the browser */}  
+    {/* <button style={styles.style} onClick={onClick}>Click me!</button> */}  
+    {/*------------------------------------------------------------------ */}  
+    {/* useRef, we can show count:0 on the browser but count value will be 1 in the memory*/}
+    {/* <button onClick={() => {count.current += 1}}>Clicks : {count.current}</button>*/}
+
+     <video 
+        width= "400"
+        controls ref={videoRef}
+        src="https://www.w3schools.com/html/mov_bbb.mp4"
+     />
+    <hr />
+    <button onClick={handlePlay}>Play</button>
     </div>
   );
 }
